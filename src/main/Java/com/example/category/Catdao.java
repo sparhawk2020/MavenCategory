5b3b6d007a9372cb9c4c62a0ef81cdef0946ab14
sparhawk2020 <cjjc20001@yahoo.com>
2022-05-15 08:14:17 -0700

    with new mockito

--- src/main/Java/com/example/category/Catdao.java
+++ src/main/Java/com/example/category/Catdao.java
@@ -1,10 +1,12 @@
 package com.example.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
@@ -16,19 +18,30 @@ import java.util.Optional;
 
 @Component
 @Service
+@Repository
 public class Catdao {
 
+@Autowired
+JdbcTemplate template;
 
-    JdbcTemplate template;
-
-    public void setTemplate(JdbcTemplate template) {
+    public Catdao(JdbcTemplate template) {
         this.template = template;
     }
 
+    /*public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }*/
 
 
 
-    public void insertData(final Category category) {
+
+
+    public int insertData(final Category category) {
+        return template.update("insert into category values(?,?)", category.getCatcode(), category.getCatdesc());
+    }
+
+
+   /* public void insertData(final Category category) {
         String query = "insert into category values(?,?)";
         template.execute(query, new PreparedStatementCallback<Boolean>() {
             @Override
@@ -40,10 +53,14 @@ public class Catdao {
         });
         System.out.println("Data inserted Successfully");
 
+    }*/
+
+
+    public int deleteData (String cat) {
+        return template.update("delete from category where catcode= ?", cat);
     }
 
-
-    public void deleteData(final String category) {
+    /*public void deleteData(final String category) {
         String query = "Delete from category where catcode = ?";
         template.execute(query, new PreparedStatementCallback<Boolean>() {
             @Override
@@ -54,10 +71,15 @@ public class Catdao {
         });
         System.out.println("Data deleted Successfully");
 
+    }*/
+
+
+
+    public int EditData(final Category category, String cat) {
+        return template.update("update category set catcode= ?, catdesc = ? where catcode = ?", category.getCatcode(), category.getCatdesc(),cat);
     }
 
-
-    public void EditData(final Category category, String cat) {
+   /* public void EditData(final Category category, String cat) {
         String query = "update category set catcode= ?, catdesc = ? where catcode = ?";
         template.execute(query, new PreparedStatementCallback<Boolean>() {
             @Override
@@ -70,7 +92,7 @@ public class Catdao {
         });
         System.out.println("Data edited Successfully");
 
-    }
+    }*/
 
     public List<Category> display() throws ClassNotFoundException, SQLException {
         //create an array list that will contain the data recovered
